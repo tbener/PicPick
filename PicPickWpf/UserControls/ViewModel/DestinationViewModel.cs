ddd009f6e811bd3162f6baf797151feb71c61dcc
Tal Bener <tbener@hotmail.com>
2019-02-27 19:24:57 +0200

    Added execution pane

--- PicPickWpf/UserControls/ViewModel/DestinationViewModel.cs
+++ PicPickWpf/UserControls/ViewModel/DestinationViewModel.cs
@@ -1,12 +1,13 @@
 ﻿using PicPick.Commands;
 using PicPick.Project;
+using PicPick.ViewModel;
 using System;
 using System.Windows.Input;
 
 namespace PicPick.UserControls.ViewModel
 {
 
-    public class DestinationViewModel
+    public class DestinationViewModel : BaseViewModel
     {
         public EventHandler OnDeleteClicked;
 
@@ -18,10 +19,26 @@ namespace PicPick.UserControls.ViewModel
             PathViewModel = new PathBrowserViewModel(Destination);
 
             DeleteCommand = new RelayCommand(delegate { OnDeleteClicked?.Invoke(this, null); });
+
+            Destination.PropertyChanged += Destination_PropertyChanged;
+        }
+
+        private void Destination_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName.Equals("Template"))
+                OnPropertyChanged("TemplatePreview");
         }
 
         public PicPickProjectActivityDestination Destination { get; set; }
 
-        public PathBrowserViewModel PathViewModel;
+        public PathBrowserViewModel PathViewModel { get; set; }
+
+        public string TemplatePreview
+        {
+            get
+            {
+                return Destination.GetTemplatePath(DateTime.Now);
+            }
+        }
     }
 }
