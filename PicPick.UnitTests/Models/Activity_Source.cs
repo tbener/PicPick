889e97aafd13babf39fb5ff20c177646fefeb6c3
Tal Bener <tbener@hotmail.com>
2019-05-23 17:42:56 +0300

    Tests - add Filter tests with auto files generator

--- PicPick.UnitTests/Models/Activity_Source.cs
+++ PicPick.UnitTests/Models/Activity_Source.cs
@@ -1,26 +1,75 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using PicPick.Models;
+using System.IO;
+using TalUtils;
 
 namespace PicPick.UnitTests.Models
 {
     /// <summary>
     /// To Test:
     /// - Filter
+    /// - IncludeSubFolders
+    /// 
+    /// For these tests we create dummy files just to test the reading by filter and subdirectories.
+    /// The files will be deleted automatically because the TestDir is deleted by MSTest.
     /// </summary>
+    [TestClass]
     public class Activity_Source
     {
-        [TestMethod]
-        public void Source_Filter_FileList(string filter, bool includeSubFolders)
+        private static string SourcePath;
+
+        
+        private static void CreateFiles(int count, string extension, string subfolder = "")
+        {
+            string dir = PathHelper.GetFullPath(SourcePath, subfolder, true) + "\\";
+            for (int i = 1; i <= count; i++)
+            {
+                File.Create(dir + i.ToString("00") + extension);
+            }
+        }
+
+        [AssemblyInitialize]
+        public static void Initialize(TestContext testContext)
+        {
+            
+            SourcePath = PathHelper.GetFullPath(testContext.TestDir, "_SourceFiles", false);
+
+            CreateFiles(10, ".jpg");
+            CreateFiles(10, ".tmp");
+            CreateFiles(5, ".jpg", "sub1");
+            CreateFiles(5, ".tmp", "sub1");
+            CreateFiles(10, ".tal");
+        }
+
+        [AssemblyCleanup]
+        public static void Cleanup()
+        {
+            //the files should be deleted by MSTest
+        }
+
+        [DataTestMethod]
+        [DataRow("*.jpg", false, 10)]
+        [DataRow("*.png", false, 0)]
+        [DataRow("*.jpg", true, 15)]
+        [DataRow("*.png", true, 0)]
+        [DataRow("*.*", false, 30)]
+        [DataRow("*.*", true, 40)]
+        [DataRow("*.jpg,*.*", false, 30)]
+        [DataRow("*.jpg  ;, *.*", false, 30)]
+        [DataRow("jpg", false, 0)]
+        [DataRow("", false, 30)]
+        [DataRow("", true, 40)]
+        [DataRow("*", true, 40)]
+        [DataRow("blahblah*.*", false, 0)]
+        [DataRow("blahblah,*.*", false, 30)]
+        [DataRow("*.jpg, *.tal", false, 20)]
+        [DataRow("*.jpg, *.tal", true, 25)]
+        public void Source_Filter_FileList(string filter, bool includeSubFolders, int expectedCount)
         {
             // arrange
             var source = new PicPickProjectActivitySource()
             {
-                Path = "",
+                Path = SourcePath,
                 Filter = filter,
                 IncludeSubFolders = includeSubFolders
             };
@@ -29,6 +78,7 @@ namespace PicPick.UnitTests.Models
             var files = source.FileList;
 
             // assert
+            Assert.AreEqual(expectedCount, files.Count);
         }
     }
 }
