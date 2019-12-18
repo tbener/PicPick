d460902ccd1ab6553bb470d843d659f1899a124e
Tal Bener <tal.bener@allscripts.com>
2019-12-18 17:27:20 +0200

    Adding DataTemplateSelector for files in different statuses

---
+++ PicPickWpf/ViewModel/UserControls/Mapping/DataTemplateSelector.cs
@@ -0,0 +1,35 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace PicPick.ViewModel.UserControls.Mapping
+{
+    public class MappingFileDataTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is MappingFileViewModel)
+            {
+                MappingFileViewModel f = item as MappingFileViewModel;
+                FrameworkElement element = container as FrameworkElement;
+                switch (f.DestinationFileInfo.Status)
+                {
+                    case Core.FILE_STATUS.NONE:
+                        // display only source file (that's the default)
+                        break;
+                    case Core.FILE_STATUS.COPIED:
+                        // display something like: [Destination File Name] (from: [Source Full Name])
+                        return element.FindResource("CopiedFile") as DataTemplate;
+                    case Core.FILE_STATUS.SKIPPED:
+                        // display only source file (default)
+                        break;
+                    case Core.FILE_STATUS.ERROR:
+                        // Source full name (error message...)
+                        return element.FindResource("ErrorFile") as DataTemplate;
+                    default:
+                        break;
+                }
+            }
+            return base.SelectTemplate(item, container);
+        }
+    }
+}
--- PicPickWpf/ViewModel/UserControls/Mapping/MappingFileViewModel.cs
+++ PicPickWpf/ViewModel/UserControls/Mapping/MappingFileViewModel.cs
@@ -13,9 +13,18 @@ namespace PicPick.ViewModel.UserControls.Mapping
         {
             FileName = file.SourceFile.FileName;
             SourceFileName = file.SourceFile.FullFileName;
+            DestinationFileName = string.IsNullOrEmpty(file.NewName) ? FileName : file.NewName;
+            Error = file.Exception?.Message;
+
+            DestinationFileInfo = file;
         }
 
         public string FileName { get; set; }
         public string SourceFileName { get; set; }
+        public string DestinationFileName { get; set; }
+        public string Error { get; set; }
+
+        public DestinationFile DestinationFileInfo { get; private set; }
     }
+
 }
